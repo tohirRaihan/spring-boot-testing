e8f21d6dcbc0651e1ba0852a4bc0431fef007806
Tohir Raihan <tohir.raihan@gmail.com>
2023-08-30 22:36:22 +0600

    :up: Update: Employee Entity class

--- src/main/java/com/tohir/springboot/entity/Employee.java
+++ src/main/java/com/tohir/springboot/entity/Employee.java
@@ -1,5 +1,23 @@
 package com.tohir.springboot.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Employee {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String email;
+
 }
